402ac8b783805ff93bf8a93caa4ce4c4734fbcab
theshatterstone <90517516+theshatterstone@users.noreply.github.com>
2022-02-11 12:49:23 +0000

    Stache is now a rectangle; key press and moving need to be added.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import time
+import random 
 #setting up pygame
 import pygame 
 from pygame.locals import *
 pygame.font.init()
-
 pygame.init()
+
 #setting up display
 screen_width = 800
 screen_height = 400
@@ -11,33 +13,58 @@ screen_height = 400
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('The Caveman') #Game title and icon go here
 
+#Change FPS
+
+FPS = 60
+
 #loading images
 
 bg_img = pygame.transform.scale(pygame.image.load('img/new-bg.jpg'), (screen_width, screen_height) )
 caveman = pygame.image.load('img/CavemanNormal.png')
-stache = pygame.image.load('img/MainChr.png')
-
+#stache = pygame.image.load('img/MainChr.png')
+screentitle = pygame.image.load('img/TitleText.png')
+# score = 
 #Load font(s)
 
 mainFont = pygame.font.SysFont('helvetica', 20)
 
+#main chr - Stache
+
+class chr:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    
+    def show(self,window):
+        pygame.draw.rect(window, (0,0,0),(self.x,self.y, 20,20))
+
+stache = chr(170, 230)
+clock = pygame.time.Clock()
 #main function
 
-def mainGame():
+def mainGame(localstache):
+    #Background image
     screen.blit(bg_img, (0,0))
     screen.blit(caveman, (50, 225))
-    screen.blit(stache, (170,230))
+#    screen.blit(stache, (170, 230))
+    localstache = chr.show(stache,screen)
+    #The Caveman Label
+    screen.blit(screentitle,(15,15))
+    # screen.blit(score,(screen_width score.getwidth() - 15, 15))
+    pygame.display.update()
 
 #run infinite loop
 run = True
 while run:
-
-    mainGame()
+    clock.tick(FPS)
+    mainGame(stache)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    pygame.display.update()
-
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]: #jump
+        stache.y -= 15
+    
 pygame.quit()
