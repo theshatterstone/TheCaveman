7650ed8c1da66a03bfd463914f77550bdbb0054e
theshatterstone <90517516+theshatterstone@users.noreply.github.com>
2022-02-11 11:16:50 +0000

    progress made

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #setting up pygame
 import pygame 
 from pygame.locals import *
+pygame.font.init()
 
 pygame.init()
 #setting up display
@@ -12,19 +13,31 @@ pygame.display.set_caption('The Caveman') #Game title and icon go here
 
 #loading images
 
-bg_img = pygame.image.load('img/blank_bg.jpg')
+bg_img = pygame.transform.scale(pygame.image.load('img/new-bg.jpg'), (screen_width, screen_height) )
 caveman = pygame.image.load('img/CavemanNormal.png')
+stache = pygame.image.load('img/MainChr.png')
+
+#Load font(s)
+
+mainFont = pygame.font.SysFont('helvetica', 20)
+
+#main function
+
+def mainGame():
+    screen.blit(bg_img, (0,0))
+    screen.blit(caveman, (50, 225))
+    screen.blit(stache, (170,230))
 
 #run infinite loop
 run = True
 while run:
 
-    screen.blit(bg_img, (0,0))
-    screen.blit(caveman, (100, 20))
+    mainGame()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
     pygame.display.update()
 
 pygame.quit()
