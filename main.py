4ea91980d35bcbd8687df125379525092a0b332c
theshatterstone <90517516+theshatterstone@users.noreply.github.com>
2022-02-15 12:10:43 +0000

    Started adding obstacles; they don't work; seems to be faulty logic, cant find it

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import time
 #setting up pygame
 import pygame 
@@ -29,6 +30,11 @@ pixelfont = pygame.font.Font('fonts/PublicPixel.ttf',20)
 
 bg_img = pygame.transform.scale(pygame.image.load('img/new-bg.jpg'), (screen_width, screen_height) )
 caveman = pygame.image.load('img/CavemanNormal.png')
+caveman2 = pygame.image.load('img/CavemanTorch.png')
+cavemanCount = 0
+toprockimg = pygame.image.load('img/Stalactite.png')
+bottomrockimg = pygame.image.load('img/Stalagmite.png')
+slipfloorimg = pygame.image.load('img/water-1(x10).png')
 stachepic = pygame.image.load('img/MainChr.png')
 screentitle = pygame.image.load('img/TitleText.png')
 intscore = 0
@@ -53,15 +59,83 @@ class chr:
     def get_width(self):
         return self.img.get_width()
 
+
+
+#obstacles classes
+'''class obstacle:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y'''
+
+class TopRock:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.img = toprockimg
+        self.mask = pygame.mask.from_surface(self.img)
+    def show(self,window):
+        window.blit(self.img, (self.x,self.y))
+    def get_height(self):
+        return self.img.get_height() 
+    def get_width(self):
+        return self.img.get_width()
+
+class BottomRock:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.img = bottomrockimg
+        self.mask = pygame.mask.from_surface(self.img)
+    def show(self,window):
+        window.blit(self.img, (self.x,self.y))
+    def get_height(self):
+        return self.img.get_height() 
+    def get_width(self):
+        return self.img.get_width()
+
+class SlipFloor:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.img = slipfloorimg
+        self.mask = pygame.mask.from_surface(self.img)
+    def show(self,window):
+        window.blit(self.img, (self.x,self.y))
+    def get_height(self):
+        return self.img.get_height() 
+    def get_width(self):
+        return self.img.get_width()
+
 stache = chr(170, 230)
+top = TopRock(random.randrange(800,850), 20)
+bottom = BottomRock(random.randrange(800,850), 225)
+slip = SlipFloor(random.randrange(800,850), 227)
 clock = pygame.time.Clock()
+
 #main function
 
-def mainGame(localstache):
+def mainGame(localstache,CMcount,localtop,localbottom,localslip):
     #Background image
     screen.blit(bg_img, (0,0))
-    screen.blit(caveman, (50, 225))
+    screen.blit(caveman2, (0, 225))
     localstache = chr.show(stache,screen)
+    randnum = random.randint(1,100)
+    '''if CMcount % 60 == 0:
+        screen.blit(caveman2, (0, 225))
+    else:
+        screen.blit(caveman, (0,225))'''
+    if randnum % 1 == 0:
+        localtop = TopRock.show(top,screen)
+    elif randnum % 3 == 0:
+        localbottom  = BottomRock.show(bottom,screen)
+    elif randnum % 7 == 0:
+        localslip = SlipFloor.show(slip,screen)
+    if localtop != top:
+        localtop.x -= x_vel
+    if localbottom != bottom:
+        localbottom.x -= x_vel
+    if localslip != slip:
+        localslip.x -= x_vel
     #The Caveman Label
     screen.blit(screentitle,(15,15))
     #screen.blit(score,((screen_width - score.get_width() - 15), 15))
@@ -76,14 +150,15 @@ def gamelose():
 run = True
 while run:
     clock.tick(FPS)
-    mainGame(stache)
-
+    mainGame(stache,cavemanCount,top,bottom,slip)
+    cavemanCount += 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             run = False
-    isjump = False
+    #Controls
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE] == True:
+    isjump = False 
+    if keys[pygame.K_SPACE] == True: # jump
         if isjump == False :
             isjump = True
         if isjump == True:
