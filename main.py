b9b56ab96378f95a4c66486a284913e872d78d43
theshatterstone <90517516+theshatterstone@users.noreply.github.com>
2022-02-16 17:21:21 +0000

    Obstacles show;they're blinking cuz they're only showing for 1 frame and they're moving cuz fixed coords R changed at 133

--- main.py
+++ main.py
@@ -32,9 +32,9 @@ bg_img = pygame.transform.scale(pygame.image.load('img/new-bg.jpg'), (screen_wid
 caveman = pygame.image.load('img/CavemanNormal.png')
 caveman2 = pygame.image.load('img/CavemanTorch.png')
 cavemanCount = 0
-toprockimg = pygame.image.load('img/Stalactite.png')
-bottomrockimg = pygame.image.load('img/Stalagmite.png')
-slipfloorimg = pygame.image.load('img/water-1(x10).png')
+toprockimg = pygame.image.load('img/Stalactite-new.png')
+bottomrockimg = pygame.image.load('img/Stalagmite-new.png')
+slipfloorimg = pygame.image.load('img/water-1(x10)-new.png')
 stachepic = pygame.image.load('img/MainChr.png')
 screentitle = pygame.image.load('img/TitleText.png')
 intscore = 0
@@ -42,7 +42,7 @@ intscore = 0
 
 #Change velocity (x and y)
 x_vel = 5 #change to 4 for game updates/optimisations/improvements
-y_vel = 20
+y_vel = 23
 
 #main chr - Stache
 
@@ -107,42 +107,43 @@ class SlipFloor:
         return self.img.get_width()
 
 stache = chr(170, 225)
-top = TopRock(random.randint(600,650), 20)
-bottom = BottomRock(random.randint(600,650), 230)
-slip = SlipFloor(random.randint(600,650), 210)
+top = TopRock(850, 50)
+bottom = BottomRock(850, 290)
+slip = SlipFloor(850, 351)
+num = 1
 clock = pygame.time.Clock()
 
 #main function
 
-def mainGame(localstache,CMcount,localtop,localbottom,localslip):
+def mainGame(num,localstache,CMcount,localtop,localbottom,localslip):
     #Background image
     screen.blit(bg_img, (0,0))
-    screen.blit(caveman2, (-30, 225))
+    #screen.blit(caveman2, (-30, 225))
     localstache = chr.show(stache,screen)
-    num = 0
-    '''if CMcount % 60 == 0:
-        screen.blit(caveman2, (0, 225))
+    if CMcount % 60 > 30:
+        screen.blit(caveman2, (-30, 225))
     else:
-        screen.blit(caveman, (0,225))'''
-    if num % 20000 == 0:
+        screen.blit(caveman, (-30,225))
+    if num % 3 == 0:
         TopRock.show(top,screen)
-    elif num % 1 == 0:
+    elif num % 5 == 0:
         BottomRock.show(bottom,screen)
-    if num % 7 == 0:
+    elif num % 7 == 0:
         SlipFloor.show(slip,screen)
-    if top.x != 0:
+    if top.x != -55:
         top.x -= x_vel
-    if bottom.x != 0:
+    if bottom.x != -60:
         bottom.x -= x_vel
-    if slip.x != 0:
+    if slip.x != -125:
         slip.x -= x_vel
     num += 1
     if keys[pygame.K_SPACE] == False and stache.y == 225:
-        time.sleep(0.05)
+        time.sleep(0.1)
     #The Caveman Label
     screen.blit(screentitle,(15,15))
     #screen.blit(score,((screen_width - score.get_width() - 15), 15))
     pygame.display.update()
+    return num 
 
 #game lose function; final score can be added here
 
@@ -163,18 +164,18 @@ while run:
         if isjump == True:
             stache.y -= y_vel
             y_vel -= 2
-            time.sleep(0.05)
-            if y_vel < -20:
+            time.sleep(0.1)
+            if y_vel < -23:
                 isjump = False
-                y_vel = 20
+                y_vel = 23
     else: 
         if stache.y != 225:
                 stache.y -= y_vel
                 y_vel -= 2
-                time.sleep(0.05)
-                if y_vel < -20:
+                time.sleep(0.1)
+                if y_vel < -23:
                     isjump = False
-                    y_vel = 20
+                    y_vel = 23
                 pygame.display.update()
     
     if keys[pygame.K_a] or keys[pygame.K_LEFT]: # left
@@ -189,9 +190,9 @@ while run:
     if keys[pygame.K_s] or keys[pygame.K_DOWN]: # right
         if stache.x + stache.get_width() < screen_width:
             stache.y = 225
-            y_vel = 20
+            y_vel = 23
 
-    mainGame(stache,cavemanCount,top,bottom,slip)
+    num = mainGame(num,stache,cavemanCount,top,bottom,slip)
     cavemanCount += 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
